97b6dd228b8493f5ca9b6961f5f49dbac27c5e5a
JosiMacGyver <joseana.stos.soares@gmail.com>
2022-11-22 08:08:57 -0300

    retomando

---
+++ src/components/básicos/Primeiro.js
@@ -0,0 +1,13 @@
+import React from "react"
+
+export default function Primeiro() {
+
+    const msg = "primeira Msg";
+
+    return(
+        <>
+        <h2>Primeiro</h2>
+        <p>{msg}</p>
+        </>
+    )
+}
